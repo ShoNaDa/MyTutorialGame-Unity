7ab5e26f327e9296870e6178ebd3c2632c973675
Sho___nada <vovaborkovskij@gmail.com>
2021-06-13 17:55:28 +0700

    Unity

---
+++ TutorialGame/Assets/Scripts/Ads.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour
+{
+    private string gameId = "", type = "video";
+    private bool testMode = true;
+}
---
+++ TutorialGame/Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasButtons : MonoBehaviour
+{
+    public Sprite musicOn, musicOff;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetString("music") == "No" && gameObject.name == "Music")
+        {
+            GetComponent<Image>().sprite = musicOff;
+        }
+    }
+
+    public void RestartGame()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadVK()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        Application.OpenURL("https://vk.com/sho____nada");
+    }
+
+    public void LoadShop()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void CloseShop()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        SceneManager.LoadScene("Main");
+    }
+
+    public void MusicWork()
+    {
+        if(PlayerPrefs.GetString("music") == "No")
+        {
+            GetComponent<AudioSource>().Play();
+            PlayerPrefs.SetString("music", "Yes");
+            GetComponent<Image>().sprite = musicOn;
+        }
+        else
+        {
+            PlayerPrefs.SetString("music", "No");
+            GetComponent<Image>().sprite = musicOff;
+        }
+    }
+}
